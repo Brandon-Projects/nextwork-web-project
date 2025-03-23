3242b17e3d6e4a9ab55cd2db7ca7342e0b59b7a8
EC2 Default User <ec2-user@ip-172-31-1-190.us-east-2.compute.internal>
2025-03-23 16:46:43 +0000

    complete documents

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,9 @@
+[Container] 2025/03/23 16:41:14.760143 Running command chmod +x run-tests.sh
+chmod: cannot access ‘run-tests.sh’: No such file or directory
+
+[Container] 2025/03/23 16:41:14.770218 Command did not exit successfully chmod +x run-tests.sh exit status 1
+[Container] 2025/03/23 16:41:14.777201 Phase complete: BUILD State: FAILED
+[Container] 2025/03/23 16:41:14.777227 Phase context status code: COMMAND_EXECUTION_ERROR Message: Error while executing command: chmod +x run-tests.sh. Reason: exit status 1
+[Container] 2025/03/23 16:41:14.811052 Entering phase POST_BUILD
+[Container] 2025/03/23 16:41:14.812177 Running command echo "Build completed on `date`"
+Build completed on Sun Mar 23 16:41:14 UTC 2025
